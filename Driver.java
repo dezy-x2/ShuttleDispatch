54652e4355c4caf1607e53f451321ea9532dc491
dezy-x2 <df.dezmond9@gmail.com>
2022-07-20 20:47:29 -0600

    added basics for Driver

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    private String driverName;
+    private boolean driverIsAvailable;
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public Driver(String driverName) {
+        this.driverName = driverName;
+        this.driverIsAvailable = true;
+    }
+
+}
